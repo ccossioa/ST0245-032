90616b480ae408074d70d7e86d9bd4a18b849079
ccossioa <52968541+ccossioa@users.noreply.github.com>
2019-09-15 20:49:06 -0500

    Add files via upload

---
+++ laboratorios/lab03/ejercicioEnLinea/Punto2.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+public class Punto2
+{
+    public static void textCompilator(String mess){
+        LinkedList list = new LinkedList();
+        String aux = "";
+        for(int i = 0; i < mess.length()-1; i++){ 
+            if ((mess.charAt(i)=='[')||(mess.charAt(i)==']')){
+                int j = i + 1;
+                int k = 0;
+                while((j<mess.length())&&!(mess.charAt(j) == '[' || mess.charAt(j) == ']')){
+                    aux = aux + mess.charAt(j);
+                    j++;
+                    k++;
+                }
+                if(mess.charAt(i) == '[') list.addFirst(aux);
+                if(mess.charAt(i) == ']') list.addLast(aux);
+                aux = "";
+                i = i + k;
+            }
+            else {
+                list.addLast(mess.charAt(i));
+            }            
+        }
+        for(int i = 0; i < list.size(); i++) System.out.print(list.get(i));
+    }
+    public static void prueba(){
+        textCompilator("[[a[[d[f[[g[g[h[h[dgd[fgsfa[f");
+    }  
+    
+    public void bloques (int n){
+        
+    }
+    
+}
